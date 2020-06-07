ccc9ffaf047b496a7a660bf5f872dbe886c62b98
MoeRookie <MoeRookie@163.com>
2020-06-07 23:12:41 +0800

    使用SP存储更新开关的状态

--- app/src/main/java/com/liqun/securitymax/activity/SettingActivity.java
+++ app/src/main/java/com/liqun/securitymax/activity/SettingActivity.java
@@ -7,6 +7,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.liqun.securitymax.R;
+import com.liqun.securitymax.utils.ConstantValue;
+import com.liqun.securitymax.utils.SpUtils;
 import com.liqun.securitymax.view.SettingItemView;
 
 public class SettingActivity extends AppCompatActivity {
@@ -26,6 +28,10 @@ public class SettingActivity extends AppCompatActivity {
      */
     private void initUpdate() {
         mSivUpdate = findViewById(R.id.siv_update);
+        // 获取已有的开关状态,用作显示
+        boolean openUpdate = SpUtils.getBoolean(this, ConstantValue.OPEN_UPDATE, false);
+        // 是否选中,根据上一次存储的结果去做决定
+        mSivUpdate.setCheck(openUpdate);
         mSivUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +39,8 @@ public class SettingActivity extends AppCompatActivity {
                 boolean isCheck = mSivUpdate.isCheck();
                 // 如果之前是选中的, 点击过后变成未选中;如果之前是未选中, 点击过后变成选中的
                 mSivUpdate.setCheck(!isCheck);
+                // 将取反后的状态存储到相应的sp中
+                SpUtils.putBoolean(getApplicationContext(),ConstantValue.OPEN_UPDATE,!isCheck);
             }
         });
     }
---
+++ app/src/main/java/com/liqun/securitymax/utils/ConstantValue.java
@@ -0,0 +1,9 @@
+package com.liqun.securitymax.utils;
+
+public class ConstantValue {
+
+    /**
+     * 是否开启更新的key
+     */
+    public static final String OPEN_UPDATE = "open_update";
+}
