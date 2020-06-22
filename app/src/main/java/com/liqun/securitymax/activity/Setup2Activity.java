75e44ce4109fc9eb05825e206803e9fb78eb2237
MoeRookie <MoeRookie@163.com>
2020-06-22 11:09:50 +0800

    添加导航界面2布局

--- app/src/main/java/com/liqun/securitymax/activity/Setup2Activity.java
+++ app/src/main/java/com/liqun/securitymax/activity/Setup2Activity.java
@@ -1,6 +1,5 @@
 package com.liqun.securitymax.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,17 +8,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.liqun.securitymax.R;
 
-public class Setup1Activity extends AppCompatActivity {
-
+public class Setup2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup_1);
+        setContentView(R.layout.activity_setup_2);
     }
 
-    public void nextPage(View view){
-        Intent intent = new Intent(this, Setup2Activity.class);
-        startActivity(intent);
-        finish();
+    public void prePage(View view){
+
     }
 }
