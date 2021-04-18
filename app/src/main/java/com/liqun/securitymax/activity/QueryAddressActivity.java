3ada5943e0efbdf82d1595f22fea5c1886591fb4
MoeRookie <MoeRookie@163.com>
2021-04-18 10:54:39 +0800

    点击查询号码归属地

--- app/src/main/java/com/liqun/securitymax/activity/QueryAddressActivity.java
+++ app/src/main/java/com/liqun/securitymax/activity/QueryAddressActivity.java
@@ -1,7 +1,15 @@
 package com.liqun.securitymax.activity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,9 +17,65 @@ import com.liqun.securitymax.R;
 import com.liqun.securitymax.engine.AddressDao;
 
 public class QueryAddressActivity extends AppCompatActivity {
+
+    private EditText mEtPhone;
+    private Button mBtnQuery;
+    private TextView mTvQueryResult;
+    private String mAddress;
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            mTvQueryResult.setText(mAddress);
+        }
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_query_address);
+
+        initUI();
+        setListener();
+    }
+
+    /**
+     * 找到我们所关心的控件
+     */
+    private void initUI() {
+        mEtPhone = findViewById(R.id.et_phone);
+        mBtnQuery = findViewById(R.id.btn_query);
+        mTvQueryResult = findViewById(R.id.tv_query_result);
+    }
+
+    /**
+     * 设置相关控件的点击事件监听
+     */
+    private void setListener() {
+        // 01. 点击查询, 注册按钮的点击事件
+        mBtnQuery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = mEtPhone.getText().toString().trim();
+                // 02. 查询是耗时操作, 开启子线程
+                query(phone);
+            }
+        });
+    }
+
+    /**
+     * 耗时操作
+     * 获取号码归属地
+     * @param phone 待查询的电话号码
+     */
+    private void query(final String phone) {
+        new Thread(){
+            @Override
+            public void run() {
+                mAddress = AddressDao.getAddress(phone);
+                // 03. 消息机制, 告知主线程查询结束, 可以去使用查询结果
+                mHandler.sendEmptyMessage(0);
+            }
+        }.start();
     }
 }
